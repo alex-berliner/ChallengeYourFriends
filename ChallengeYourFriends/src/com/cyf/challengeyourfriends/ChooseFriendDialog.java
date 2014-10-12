0ed59a567f3a184691ffda82e1239a8347972e32
Monorail <alexberliner@gmail.com>
2014-10-12 13:46:41 -0400

    More UI areas

---
+++ ChallengeYourFriends/src/com/cyf/challengeyourfriends/ChooseFriendDialog.java
@@ -0,0 +1,7 @@
+package com.cyf.challengeyourfriends;
+
+import android.app.DialogFragment;
+
+public class ChooseFriendDialog extends DialogFragment{
+
+}
--- ChallengeYourFriends/src/com/cyf/challengeyourfriends/LoginActivity.java
+++ ChallengeYourFriends/src/com/cyf/challengeyourfriends/LoginActivity.java
@@ -1,21 +1,23 @@
 package com.cyf.challengeyourfriends;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.LoaderManager.LoaderCallbacks;
-import android.content.ContentResolver;
+import android.content.Context;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
-import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -25,8 +27,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import java.util.ArrayList;
-import java.util.List;
+import android.widget.Toast;
 
 /**
  * A login screen that offers login via email/password.
@@ -95,6 +96,9 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 	 * errors are presented and no actual login attempt is made.
 	 */
 	public void attemptLogin() {
+		Intent intent = new Intent(this, MenuActivity.class);
+	    startActivity(intent);
+	    /*
 		if (mAuthTask != null) {
 			return;
 		}
@@ -139,6 +143,7 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
 			mAuthTask = new UserLoginTask(email, password);
 			mAuthTask.execute((Void) null);
 		}
+		*/
 	}
 
 	private boolean isEmailValid(String email) {
